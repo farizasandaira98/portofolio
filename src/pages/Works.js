49311704b9b00e5ef8045a48698330f8f673df8e
Muh Fariza <muh.fariza@tsmid.com>
2024-06-19 09:40:31 +0700

    add menu works

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,43 +1,66 @@
-import { Col, Row } from "reactstrap";
+import { Card, CardBody, CardHeader, Col, Row } from "reactstrap";
+import { motion } from "framer-motion";
 import NavbarMenu from "../component/Navbar";
+import { containerVariants } from "../styles/HomeStyle";
 
 function Works() {
     return (
-        <div className="">
+        <>
             <NavbarMenu />
-            <Row className="mt-3 d-flex align-items-center justify-content-between flex-column">
-                <Col className="d-flex align-items-center justify-content-center" lg={6} md={6} sm={12}>
-                    <h1 className="fw-bold text-center">
-                        What I Do
-                    </h1>
-                </Col>
-                <Col className="d-flex align-items-center justify-content-center mt-3" lg={6} md={6} sm={12}>
-                    <Col lg={4} md={4} sm={12}>
-                    <h3 className="fw-bold">
-                        Front End
-                    </h3>
-                    <p>
-                        Test 1 2 3
-                    </p>
+            <motion.div className="p-5" variants={containerVariants} initial="hidden" animate="visible">
+                <Row className="mt-3 d-flex align-items-center justify-content-center flex-column">
+                    <Col className="d-flex align-items-center justify-content-center" lg={6} md={6} sm={12}>
+                        <h1 className="fw-bold text-center">
+                            What I Do
+                        </h1>
                     </Col>
-                    <Col lg={4} md={4} sm={12}>
-                    <h3 className="fw-bold">
-                        Back End
-                    </h3>
-                    </Col>
-                    <Col lg={4} md={4} sm={12}>
-                    <h3 className="fw-bold">
-                        Database
-                    </h3>
-                    </Col>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
+                    <Col className="d-flex align-items-center justify-content-between mt-5" lg={6} md={6} sm={12}>
+                        <Card className="shadow" style={{ marginRight: "30px"}}>
+                            <CardHeader>
+                                <h3 className="fw-bold text-center">
+                                    Front End
+                                </h3>
+                            </CardHeader>
+                            <CardBody>
+                                <p className="text-muted fw-bold text-center">
+                                    Membuat Halaman Depan Web Yang Responsive.
+                                </p>
+                            </CardBody>
+                        </Card>
+                        <Card className="shadow" style={{ marginRight: "30px"}}>
+                            <CardHeader>
+                                <h3 className="fw-bold text-center">
+                                    Front End
+                                </h3>
+                            </CardHeader>
+                            <CardBody>
+                                <p className="text-muted fw-bold text-center">
+                                    Membuat Halaman Depan Web Yang Responsive.
+                                </p>
+                            </CardBody>
+                        </Card>
 
-                </Col>
-            </Row>
-        </div>
+                        <Card className="shadow">
+                            <CardHeader>
+                                <h3 className="fw-bold text-center">
+                                    Front End
+                                </h3>
+                            </CardHeader>
+                            <CardBody>
+                                <p className="text-muted fw-bold text-center">
+                                    Membuat Halaman Depan Web Yang Responsive.
+                                </p>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </motion.div>
+        </>
     )
 }
 
